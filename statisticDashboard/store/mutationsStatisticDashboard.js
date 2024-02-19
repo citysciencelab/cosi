7e76c539e9dac825d9873ca123414a9be894ff53
Feng Feng <feng.feng@gv.hamburg.de>
2024-02-19 23:59:59 +0100

    G31DEV1-1929 Remove: statisticDashboard module from addons.

--- statisticDashboard/store/mutationsStatisticDashboard.js
+++
@@ -1,8 +0,0 @@
-import {generateSimpleMutations} from "../../../src/app-store/utils/generators";
-import initialState from "./stateStatisticDashboard";
-
-const mutations = {
-    ...generateSimpleMutations(initialState)
-};
-
-export default mutations;
